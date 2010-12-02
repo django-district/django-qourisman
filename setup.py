b5daa0d80418ec38442ed9f0064c3ca0e58f3ed5
Chris Adams <chris@improbable.org>
2010-12-02 15:44:59 -0500

    Fixed setup.py to install qourisman.auth

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     url='https://github.com/django-district/django-qourisman',
     license='http://code.djangoproject.com/browser/django/trunk/LICENSE',
-    packages=['qourisman', 'qourisman.filterspecs'],
+    packages=['qourisman', 'qourisman.filterspecs', 'qourisman.auth'],
     description='A collection of Django admin extensions',
     # test_suite="tests.all_tests",
 )
