96625b9fecbb2997df063ec1e4860f9ab08fa093
Chris Adams <chris@improbable.org>
2011-09-12 10:54:42 -0400

    Bumped version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-qourisman',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/django-district/django-qourisman',
     license='http://code.djangoproject.com/browser/django/trunk/LICENSE',
     packages=['qourisman', 'qourisman.filterspecs', 'qourisman.auth'],
