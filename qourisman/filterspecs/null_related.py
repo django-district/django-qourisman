62aa2119d2de93a0bc2841698044a65e1afd52da
Chris Adams <chris@improbable.org>
2012-02-16 16:54:41 -0500

    Add NullChoicesFilterSpec
    * Add NullChoicesFilterSpec which makes admin changelist filters display 'All',
      'Unspecified' and then the in-use values
    * Bumped version to 0.0.4

--- qourisman/filterspecs/null_related.py
+++ qourisman/filterspecs/null_related.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.contrib.admin.filterspecs import FilterSpec, RelatedFilterSpec
+from django.contrib.admin.filterspecs import FilterSpec, RelatedFilterSpec, ChoicesFilterSpec
 from django.db import models
 from django.utils.encoding import smart_unicode
 from django.utils.translation import ugettext_lazy as _
@@ -38,6 +38,49 @@ class NullValueFilter(RelatedFilterSpec):
             'query_string': cl.get_query_string({self.lookup_kwarg: True}, [self.lookup_kwarg]),
             'display': _('Unassigned')
         }
+
+
+class NullChoicesFilterSpec(ChoicesFilterSpec):
+    """
+    This is similar to the built-in related choices but includes an option to
+    select NULL values
+    """
+
+    def __init__(self, f, request, params, model, model_admin,
+                 field_path=None):
+        super(NullChoicesFilterSpec, self).__init__(f, request, params, model, model_admin, field_path=field_path)
+        self.lookup_null_kwarg = '%s__isnull' % self.field_path
+        self.lookup_null = request.GET.get(self.lookup_null_kwarg, False)
+
+    def has_output(self):
+        # Force this to be true so we'll display the All and Unspecified
+        # options even if there are no other choices:
+        return True
+
+    def choices(self, cl):
+        i = super(NullChoicesFilterSpec, self).choices(cl)
+
+        i.next()  # Discard "All" choice since we need our additional check for !NULL
+
+        yield {
+            'selected': self.lookup_val is None and not self.lookup_null,
+            'query_string': cl.get_query_string({}, [self.lookup_kwarg, self.lookup_null_kwarg]),
+            'display': _('All')
+        }
+
+        yield {
+            'selected': self.lookup_val is None and self.lookup_null,
+            'query_string': cl.get_query_string({self.lookup_null_kwarg: True}, [self.lookup_kwarg]),
+            'display': _('Unspecified')
+        }
+
+        for j in i:
+            yield j
+
+    @classmethod
+    def can_handle_field(cls, f):
+        print f.name
+        return getattr(f, "null_choices_filter", False)
 
 
 class NullRelatedFilterSpec(RelatedFilterSpec):
@@ -93,6 +136,10 @@ class NullRelatedFilterSpec(RelatedFilterSpec):
 
 FilterSpec.filter_specs.insert(0,
     (NullRelatedFilterSpec.can_handle_field, NullRelatedFilterSpec)
+)
+
+FilterSpec.filter_specs.insert(0,
+    (NullChoicesFilterSpec.can_handle_field, NullChoicesFilterSpec)
 )
 
 FilterSpec.filter_specs.insert(0,
