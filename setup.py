81a2d5df4322e51d27d44894824e6ccdd317c34c
Tong Wang <twan@loc.gov>
2012-03-01 11:39:43 -0500

    update setup.py to include templates

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version='0.0.5',
     url='https://github.com/django-district/django-qourisman',
     license='http://code.djangoproject.com/browser/django/trunk/LICENSE',
-    packages=['qourisman', 'qourisman.filterspecs', 'qourisman.auth', 'qourisman.templatetags'],
+    packages=find_packages(),
     data_files=[('qourisman/templates/admin', template_files)],
     description='A collection of Django admin extensions',
     # test_suite="tests.all_tests",
