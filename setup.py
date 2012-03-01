487bbac66702efeb390d4cee71f110cf7b0f76c1
Tong Wang <twan@loc.gov>
2012-03-01 12:35:24 -0500

    another try to include templates

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup, find_packages
-import glob
 
-template_files = glob.glob("qourisman/templates/admin/*")
-print template_files
+template_patterns = [
+    'templates/*.html',
+    'templates/*/*.html',
+    'templates/*/*/*.html',
+    ]
+
+packages = find_packages()
 
 setup(
     name='django-qourisman',
     version='0.0.5',
     url='https://github.com/django-district/django-qourisman',
     license='http://code.djangoproject.com/browser/django/trunk/LICENSE',
-    packages=find_packages(),
-    data_files=[('qourisman/templates/admin', template_files)],
+    packages=packages,
+    package_data=dict( (package_name, template_patterns)
+                   for package_name in packages ),
     description='A collection of Django admin extensions',
     # test_suite="tests.all_tests",
 )
