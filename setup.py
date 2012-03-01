dc69a258eac314418cb6d2e5cc30d02f40b160c1
Chris Adams <chris@improbable.org>
2012-03-01 15:08:06 -0500

    setup.py: ensure that package data is installed
    We actually need our HTML templates, not just Python modules anymore

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     url='https://github.com/django-district/django-qourisman',
     license='http://code.djangoproject.com/browser/django/trunk/LICENSE',
     packages=packages,
+    zip_safe=False,
+    include_package_data=True,
     description='A collection of Django admin extensions',
     # test_suite="tests.all_tests",
 )
