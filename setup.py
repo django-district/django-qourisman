3215468458ed9ec3530b1083474e500672458039
Tong Wang <twan@loc.gov>
2012-03-01 13:55:57 -0500

    use MANIFEST.in to include templates

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup, find_packages
 
-template_patterns = [
-    'templates/*.html',
-    'templates/*/*.html',
-    'templates/*/*/*.html',
-    ]
-
 packages = find_packages()
 
 setup(
@@ -14,8 +8,6 @@ setup(
     url='https://github.com/django-district/django-qourisman',
     license='http://code.djangoproject.com/browser/django/trunk/LICENSE',
     packages=packages,
-    package_data=dict( (package_name, template_patterns)
-                   for package_name in packages ),
     description='A collection of Django admin extensions',
     # test_suite="tests.all_tests",
 )
