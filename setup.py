c5cdba5c9b5abdd965b8999d67c08bbcf06639d7
Chris Adams <chris@improbable.org>
2012-03-01 16:34:35 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ packages = find_packages()
 
 setup(
     name='django-qourisman',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/django-district/django-qourisman',
     license='http://code.djangoproject.com/browser/django/trunk/LICENSE',
     packages=packages,
